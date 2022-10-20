82e1fac06d477d90bbd43249934720ed1a4b4643
malk04 <20nastya0457@gmail.com>
2022-10-20 18:41:40 +0300

    write to file

--- laba4/src/Main.java
+++ laba4/src/Main.java
@@ -1,5 +1,14 @@
+// 21ВП2 Малькова, Лакеева
+// Вариант 8. Лаба 4
+// Напишите функцию проверки вхождения списка L1 в список L2. Результат сохранить в файл. Создать итератор для коллекции.
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main{
     public static boolean includesAll(ArrayList l1, ArrayList l2) {
@@ -22,29 +31,63 @@ public class Main{
     }
 
     public static void main(String[] args) {
-        // первый список
-        ArrayList<String> L2 = new ArrayList<>();
-        L2.add("one");
-        L2.add("two");
-        L2.add("three");
-        L2.add("four");
-        L2.add("five");
 
-        //второй список
+        System.out.println("Введите 1-й список через запятую и пробел: ");
+        Scanner in1 = new Scanner(System.in);
+        String l1 = in1.nextLine();
+        boolean check = Pattern.matches("^[^, ]+(, [^, \\n]+)*$", l1);
+        while (!check){
+            System.out.println("Введите элементы списка через одну запятую и один пробел");
+            l1 = in1.nextLine();
+            check = Pattern.matches("^[^, ]+(, [^, \\n]+)*$", l1);
+        }
+        Pattern p = Pattern.compile(", ");
+        String[] list1 = p.split(l1);
         ArrayList<String> L1 = new ArrayList<>();
-        L1.add("two");
-        L1.add("three");
-        L1.add("four");
+        Collections.addAll(L1, list1);
 
-        // результат
-        System.out.println("Результат: " + includesAll(L1, L2));
-        System.out.println("Метод containsAll: " + L2.containsAll(L1));
+        System.out.println("Введите 2-й список через запятую и пробел: ");
+        Scanner in2 = new Scanner(System.in);
+        String l2 = in2.nextLine();
+        check = Pattern.matches("^[^, ]+(, [^, \\n]+)*$", l2);
+        while (!check){
+            System.out.println("Введите элементы списка через одну запятую и один пробел");
+            l2 = in2.nextLine();
+            check = Pattern.matches("^[^, ]+(, [^, \\n]+)*$", l2);
+        }
+        String[] list2 = p.split(l2);
+        ArrayList<String> L2 = new ArrayList<>();
+        Collections.addAll(L2, list2);
 
-        // добавим в первый список элемент, которого нет во втором
-        L1.add("ten");
+        // второй список
+//        ArrayList<String> L2 = new ArrayList<>();
+//        L2.add("one");
+//        L2.add("two");
+//        L2.add("three");
+//        L2.add("four");
+//        L2.add("five");
 
-        // результат
-        System.out.println("Результат: " + includesAll(L1, L2));
-        System.out.println("Метод containsAll: " + L2.containsAll(L1));
+        // первый список
+//        ArrayList<String> L1 = new ArrayList<>();
+//        L1.add("two");
+//        L1.add("three");
+//        L1.add("four");
+
+        try(FileWriter writer = new FileWriter("results.txt", true))
+        {
+            String joined1 = String.join(", ", L1);
+            String joined2 = String.join(", ", L2);
+            String text;
+            if (includesAll(L1, L2)){
+                text = joined1 + "  ->  " + joined2 + "  ->  L1 входит в L2";
+            } else {
+                text = joined1 + "  ->  " + joined2 + "  ->  L1 не входит в L2";
+            }
+            writer.write(text);
+            writer.append('\n');
+        }
+        catch(IOException e){
+            System.out.println("Error: " + e);
+        }
     }
 }
