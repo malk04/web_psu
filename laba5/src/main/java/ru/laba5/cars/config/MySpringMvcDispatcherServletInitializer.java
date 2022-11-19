31f813e9d605c89de7c8119b8556638f5a46b48e
malk04 <20nastya0457@gmail.com>
2022-11-19 10:49:21 +0300

    spring boot

---
+++ laba5/src/main/java/ru/laba5/cars/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,17 @@
+/*package ru.laba5.cars.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}*/
